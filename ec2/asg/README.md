65b42dbb6863613d67ba8f233364abc64ef8e768
itsdalmo <kristian@doingit.no>
2017-08-09 15:28:04 +0200

    Added module for autoscaling groups.

---
+++ ec2/asg/README.md
@@ -0,0 +1,69 @@
+## ec2/asg
+
+Easy way of setting up an autoscaling group, which also takes care of creating:
+
+- Launch configuration.
+- Security group (including public egress).
+- IAM role with instance profile.
+
+```hcl
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+module "vpc" {
+  source        = "github.com/itsdalmo/tf-modules//ec2/vpc"
+  prefix        = "your-project"
+  environment   = "dev"
+  cidr_block    = "10.8.0.0/16"
+  dns_hostnames = "true"
+}
+
+module "asg" {
+  source        = "github.com/itsdalmo/tf-modules//ec2/asg"
+  prefix          = "your-project"
+  environment     = "dev"
+  user_data       = "#!bin/bash\necho hello world"
+  vpc_id          = "${module.vpc.vpc_id}"
+  subnet_ids      = "${module.vpc.subnet_ids}"
+  instance_policy = "${data.aws_iam_policy_document.permissions.json}"
+}
+
+resource "aws_security_group_rule" "ingress" {
+  security_group_id = "${module.asg.sg_id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+data "aws_iam_policy_document" "permissions" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:AssociateAddress",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "subnet_ids" {
+  value = "${module.vpc.subnet_ids}"
+}
+
+output "sg_id" {
+  value = "${module.asg.sg_id}"
+}
+
+output "role_arn" {
+  value = "${module.asg.role_arn}"
+}
+```
---
+++ ec2/asg/asg.tf
@@ -0,0 +1,157 @@
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+variable "prefix" {
+  description = "A prefix used for naming resources."
+}
+
+variable "environment" {
+  description = "Environment tag which is applied to resources."
+  default     = ""
+}
+
+variable "user_data" {
+  description = "User data script for the launch configuration."
+  default     = ""
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC for the subnets."
+}
+
+variable "subnet_ids" {
+  description = "ID of subnets where bastion can be provisioned."
+  type        = "list"
+}
+
+variable "instance_type" {
+  description = "Type of instance to provision."
+  default     = "t2.micro"
+}
+
+variable "instance_count" {
+  description = "Desired (and minimum) number of instances."
+  default     = "1"
+}
+
+variable "instance_ami" {
+  description = "AMI id for the launch configuration."
+  default     = "ami-d7b9a2b1"
+}
+
+variable "instance_key" {
+  description = "Name of an EC2 key-pair for SSH access."
+  default     = ""
+}
+
+variable "instance_policy" {
+  description = "A policy document which is applied to the instance profile."
+}
+
+# ------------------------------------------------------------------------------
+# Resources
+# ------------------------------------------------------------------------------
+resource "aws_iam_role" "main" {
+  name               = "${var.prefix}-role"
+  assume_role_policy = "${data.aws_iam_policy_document.main.json}"
+}
+
+data "aws_iam_policy_document" "main" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = "${var.prefix}-profile"
+  role = "${aws_iam_role.main.name}"
+}
+
+resource "aws_iam_role_policy" "main" {
+  name   = "${var.prefix}-permissions"
+  role   = "${aws_iam_role.main.id}"
+  policy = "${var.instance_policy}"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.prefix}-sg"
+  description = "Terraformed security group."
+  vpc_id      = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.prefix}-sg"
+    terraform   = "true"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_security_group_rule" "egress" {
+  security_group_id = "${aws_security_group.main.id}"
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_launch_configuration" "main" {
+  name_prefix          = "${var.prefix}-config-"
+  instance_type        = "${var.instance_type}"
+  iam_instance_profile = "${aws_iam_instance_profile.main.name}"
+  security_groups      = ["${aws_security_group.main.id}"]
+  image_id             = "${var.instance_ami}"
+  key_name             = "${var.instance_key}"
+  user_data            = "${var.user_data}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  name                 = "${aws_launch_configuration.main.name}"
+  desired_capacity     = "${var.instance_count}"
+  min_size             = "${var.instance_count}"
+  max_size             = "${var.instance_count + 1}"
+  launch_configuration = "${aws_launch_configuration.main.name}"
+  vpc_zone_identifier  = ["${var.subnet_ids}"]
+
+  tag {
+    key                 = "Name"
+    value               = "${var.prefix}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "environment"
+    value               = "${var.environment}"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ------------------------------------------------------------------------------
+# Output
+# ------------------------------------------------------------------------------
+output "role_arn" {
+  value = "${aws_iam_role.main.arn}"
+}
+
+output "sg_id" {
+  value = "${aws_security_group.main.id}"
+}
