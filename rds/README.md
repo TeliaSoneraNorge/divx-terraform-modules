7b825e3b796df062adc4998c433c94b978a3d598
itsdalmo <kristian@doingit.no>
2017-08-09 21:43:52 +0200

    Added RDS wrapper

---
+++ rds/README.md
@@ -0,0 +1,55 @@
+## rds
+
+Module for creating an RDS instance in a VPC, with a subnet group and
+security group with public egress.
+
+```hcl
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+module "vpc" {
+  source        = "github.com/itsdalmo/tf-modules//ec2/vpc"
+  prefix        = "your-project"
+  environment   = "dev"
+  cidr_block    = "10.8.0.0/16"
+  dns_hostnames = "true"
+}
+
+module "rds" {
+  source        = "github.com/itsdalmo/tf-modules//rds"
+
+  prefix        = "your-project"
+  environment   = "dev"
+  rds_user      = "someuser"
+  rds_password  = "<kms-encrypted-password>"
+  rds_port      = "5000"
+  rds_engine    = "postgres"
+  instance_type = "db.m3.medium"
+  storage_size  = "50"
+  vpc_id        = "${module.vpc.vpc_id}"
+  subnet_ids    = "${module.vpc.subnet_ids}"
+}
+
+resource "aws_security_group_rule" "bastion_ingress" {
+  security_group_id        = "${module.rds.security_group_id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "${module.rds.port}"
+  to_port                  = "${module.rds.port}"
+  source_security_group_id = "<bastion-sg-id>"
+}
+
+output "security_group_id" {
+  value = "${module.rds.security_group_id}"
+}
+
+output "address" {
+  value = "${module.rds.address}"
+}
+
+output "port" {
+  value = "${module.rds.port}"
+}
+```
---
+++ rds/rds.tf
@@ -0,0 +1,165 @@
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+variable "prefix" {
+  description = "A prefix used for naming resources."
+}
+
+variable "environment" {
+  description = "Environment tag which is applied to resources."
+  default     = ""
+}
+
+variable "rds_user" {
+  description = "RDS username."
+}
+
+variable "rds_password" {
+  description = "RDS password (KMS Encrypted)."
+}
+
+variable "rds_port" {
+  description = "RDS port."
+  default     = "5439"
+}
+
+variable "rds_engine" {
+  description = "Type of DB engine."
+  default     = "postgres"
+}
+
+variable "instance_type" {
+  description = "Type of DB instance to provision."
+  default     = "db.m3.medium"
+}
+
+variable "storage_size" {
+  description = "Storage allocated for the DB."
+  default     = "50"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC for the subnets."
+}
+
+variable "subnet_ids" {
+  description = "ID of subnets for the RDS subnet group."
+  type        = "list"
+}
+
+variable "public_access" {
+  description = "Flag whether the DB should be publicly accessible."
+  default     = "false"
+}
+
+variable "skip_snapshot" {
+  description = "Flag whether to skip the final snapshot."
+  default     = "true"
+}
+
+# -------------------------------------------------------------------------------
+# Resources
+# -------------------------------------------------------------------------------
+data "aws_kms_secret" "decrypted" {
+  secret {
+    name    = "rds_password"
+    payload = "${var.rds_password}"
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier             = "${var.prefix}-db"
+  name                   = "main"
+  username               = "${var.rds_user}"
+  password               = "${data.aws_kms_secret.decrypted.rds_password}"
+  port                   = "${var.rds_port}"
+  engine                 = "${var.rds_engine}"
+  instance_class         = "${var.instance_type}"
+  storage_type           = "gap2"
+  allocated_storage      = "${var.storage_size}"
+  skip_final_snapshot    = "${var.skip_snapshot}"
+  publicly_accessible    = "${var.public_access}"
+  vpc_security_group_ids = ["${aws_security_group.main.id}"]
+  multi_az               = "true"
+
+  # NOTE: This is duplicated because subnet_group does not return the name.
+  db_subnet_group_name = "${var.prefix}-subnet-group"
+
+  tags {
+    Name        = "${var.prefix}-db"
+    terraform   = "true"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name        = "${var.prefix}-subnet-group"
+  description = "Terraformed subnet group."
+  subnet_ids  = ["${var.subnet_ids}"]
+
+  tags {
+    Name        = "${var.prefix}-subnet-group"
+    terraform   = "true"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.prefix}-sg"
+  description = "Terraformed security group."
+  vpc_id      = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.prefix}-sg"
+    terraform   = "true"
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_security_group_rule" "egress" {
+  security_group_id = "${aws_security_group.main.id}"
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# ------------------------------------------------------------------------------
+# Output
+# ------------------------------------------------------------------------------
+output "id" {
+  value = "${aws_db_instance.main.id}"
+}
+
+output "arn" {
+  value = "${aws_db_instance.main.arn}"
+}
+
+output "address" {
+  value = "${aws_db_instance.main.address}"
+}
+
+output "endpoint" {
+  value = "${aws_db_instance.main.endpoint}"
+}
+
+output "port" {
+  value = "${aws_db_instance.main.port}"
+}
+
+output "database" {
+  value = "${aws_db_instance.main.name}"
+}
+
+output "security_group_id" {
+  value = "${aws_security_group.main.id}"
+}
+
+output "subnet_group_id" {
+  value = "${aws_db_subnet_group.main.id}"
+}
+
+output "subnet_group_arn" {
+  value = "${aws_db_subnet_group.main.arn}"
+}
