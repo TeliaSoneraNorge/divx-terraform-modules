1e3135cfb0efd520731c10e5674caa79daf1ed7b
itsdalmo <kristian@doingit.no>
2017-08-17 23:31:23 +0200

    Split statements into two policies.

--- iam/developer/role.tf
+++ iam/developer/role.tf
@@ -32,6 +32,31 @@ resource "aws_iam_role_policy" "protect_role" {
 }
 
 data "aws_iam_policy_document" "protect_role" {
+  # NOTE: Disallow users from making changes to the developer role and policies.
+  statement {
+    effect = "Deny"
+
+    not_actions = [
+      "iam:GenerateServiceLastAccessedDetails",
+      "iam:ListInstanceProfilesForRole",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListRolePolicies",
+      "iam:GetRolePolicy",
+    ]
+
+    resources = [
+      "${module.role.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "inspect_role" {
+  name   = "${var.prefix}-inspect-role"
+  role   = "${module.role.name}"
+  policy = "${data.aws_iam_policy_document.inspect_role.json}"
+}
+
+data "aws_iam_policy_document" "inspect_role" {
   # NOTE: We allow reading the policies so users can better understand the roles scope.
   statement {
     effect = "Allow"
@@ -54,23 +79,6 @@ data "aws_iam_policy_document" "protect_role" {
 
     resources = [
       "*"
-    ]
-  }
-
-  # NOTE: Disallow users from making changes to the developer role and policies.
-  statement {
-    effect = "Deny"
-
-    not_actions = [
-      "iam:GenerateServiceLastAccessedDetails",
-      "iam:ListInstanceProfilesForRole",
-      "iam:ListAttachedRolePolicies",
-      "iam:ListRolePolicies",
-      "iam:GetRolePolicy",
-    ]
-
-    resources = [
-      "${module.role.arn}",
     ]
   }
 }
