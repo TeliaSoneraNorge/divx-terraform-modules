4ecf6e3fbb9c1a082f81aa3dd70b2415786ba52b
itsdalmo <kristian@doingit.no>
2017-08-21 11:53:35 +0200

    First draft of restricted EC2 policy.

--- iam/policy/ec2/policy.tf
+++ iam/policy/ec2/policy.tf
@@ -5,6 +5,11 @@ variable "prefix" {
   description = "A prefix used for naming resources."
 }
 
+variable "resources" {
+  description = "Restrict access to specific resources. Defaults to 'prefix-*'."
+  default     = ""
+}
+
 variable "account_id" {
   description = "Restrict access to a given account ID."
 }
@@ -27,22 +32,119 @@ resource "aws_iam_role_policy" "main" {
 }
 
 data "aws_iam_policy_document" "main" {
+  # NOTE: The instance profiles which can be passed is limited to the prefix by `iam:PassRole`.
+  statement {
+    effect = "Allow"
+
+    not_actions = [
+      "ec2:CreateVolume",
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:RunInstances",
+    ]
+
+    resources = [
+      "arn:aws:ec2:${var.region}:${var.account_id}:*/*",
+      "arn:aws:ec2:${var.region}::image/*",
+      "arn:aws:ec2:${var.region}::snapshot/*",
+    ]
+
+    condition = {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+    }
+  }
+
   statement {
     effect = "Allow"
 
     actions = [
-      "ec2:*",
+      "ec2:CreateTags",
     ]
 
     resources = [
-      "*",
+      "arn:aws:ec2:${var.region}:${var.account_id}:*/*",
+      "arn:aws:ec2:${var.region}::image/*",
+      "arn:aws:ec2:${var.region}::snapshot/*",
     ]
 
-    # TODO: Restrict access to specific resource names if possible.
     condition = {
-      test     = "StringEquals"
-      variable = "ec2:Region"
-      values   = ["${var.region}"]
+      test     = "StringLikeIfExists"
+      variable = "ec2:ResourceTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+    }
+
+    condition = {
+      test     = "StringLikeIfExists"
+      variable = "aws:RequestTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:DeleteTags",
+    ]
+
+    resources = [
+      "arn:aws:ec2:${var.region}:${var.account_id}:*/*",
+      "arn:aws:ec2:${var.region}::image/*",
+      "arn:aws:ec2:${var.region}::snapshot/*",
+    ]
+
+    condition = {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+    }
+
+    condition = {
+      test     = "ForAllValues:StringNotEquals"
+      variable = "aws:TagKeys"
+      values   = ["Name"]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:RunInstances",
+    ]
+
+    resources = [
+      "arn:aws:ec2:${var.region}:${var.account_id}:*/*",
+      "arn:aws:ec2:${var.region}::image/*",
+      "arn:aws:ec2:${var.region}::snapshot/*",
+    ]
+
+    condition = {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:RunInstances",
+    ]
+
+    # NOTE: Must cover creating instance/volume with tag in request.
+    resources = [
+      "arn:aws:ec2:${var.region}:${var.account_id}:instance/*",
+      "arn:aws:ec2:${var.region}:${var.account_id}:volume/*",
+    ]
+
+    condition = {
+      test     = "StringLike"
+      variable = "aws:RequestTag/Name"
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
     }
   }
 }
