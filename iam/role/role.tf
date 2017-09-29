cd3cf5fd3e732588e12cc9d447f2669cdf43cc27
itsdalmo <kristian@doingit.no>
2017-09-29 14:37:09 +0200

    Include display name in assume role link.

--- iam/role/role.tf
+++ iam/role/role.tf
@@ -69,5 +69,5 @@ output "arn" {
 }
 
 output "url" {
-  value = "https://signin.aws.amazon.com/switchrole?account=${data.aws_iam_account_alias.current.account_alias}&roleName=${aws_iam_role.main.name}"
+  value = "https://signin.aws.amazon.com/switchrole?account=${data.aws_iam_account_alias.current.account_alias}&roleName=${aws_iam_role.main.name}&displayName=${var.prefix}%20@%20${data.aws_iam_account_alias.current.account_alias}"
 }
