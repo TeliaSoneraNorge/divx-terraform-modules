4a8106895b8c2459cd1bcc0436a71ae2f47b2eb6
itsdalmo <kristian@doingit.no>
2017-10-02 20:53:42 +0200

    Start reworking IAM policies.

---
+++ iam/policies/policies.tf
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+variable "prefix" {
+  description = "A prefix used for naming resources."
+}
+
+variable "resources" {
+  description = "Restrict access to specific resources. Defaults to 'prefix-*'."
+  default     = ""
+}
+
+variable "account_id" {
+  description = "Restrict access to a given account ID."
+}
+
+variable "region" {
+  description = "Restrict privileges to a given region."
+}
+
+variable "iam_role_name" {
+  description = "Name of IAM role to attach the generated policy to."
+}
+
+# ------------------------------------------------------------------------------
+# Resources
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Output
+# ------------------------------------------------------------------------------
