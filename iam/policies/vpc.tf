10c21491a20a14a3af45979ba007a79e529d88ad
Anton Johansson <antjoh91@gmail.com>
2017-11-07 17:24:14 +0200

    Fix tag errors and removed unnecessary actions

--- iam/policies/vpc.tf
+++ iam/policies/vpc.tf
@@ -2,36 +2,25 @@
 # Resources
 # ------------------------------------------------------------------------------
 resource "aws_iam_role_policy" "vpc" {
-  count  = "${contains(var.services, "ec2") && var.iam_role_name != "" ? 1 : 0}"
+  count  = "${contains(var.services, "vpc") && var.iam_role_name != "" ? 1 : 0}"
   name   = "${var.prefix}-vpc-policy"
   role   = "${var.iam_role_name}"
   policy = "${data.aws_iam_policy_document.ec2.json}"
 }
 
 resource "aws_iam_user_policy" "vpc" {
-  count  = "${contains(var.services, "ec2") && var.iam_user_name != "" ? 1 : 0}"
+  count  = "${contains(var.services, "vpc") && var.iam_user_name != "" ? 1 : 0}"
   name   = "${var.prefix}-vpc-policy"
   user   = "${var.iam_role_name}"
   policy = "${data.aws_iam_policy_document.vpc.json}"
 }
 
 data "aws_iam_policy_document" "vpc" {
-  statement {
-    effect = "Allow"
 
-    actions = [
-                "ec2:AcceptVpcPeeringConnection",
-                "ec2:AllocateAddress",
-                "ec2:AssignPrivateIpAddresses",
-                "ec2:AssociateAddress",
-                "ec2:AssociateDhcpOptions",
-                "ec2:AssociateRouteTable",
-                "ec2:AttachClassicLinkVpc",
-                "ec2:AttachInternetGateway",
-                "ec2:AttachNetworkInterface",
-                "ec2:AttachVpnGateway",
-                "ec2:AuthorizeSecurityGroupEgress",
-                "ec2:AuthorizeSecurityGroupIngress",
+  statement {
+      effect = "Allow"
+
+      actions = [
                 "ec2:CreateCustomerGateway",
                 "ec2:CreateDhcpOptions",
                 "ec2:CreateFlowLogs",
@@ -43,15 +32,41 @@ data "aws_iam_policy_document" "vpc" {
                 "ec2:CreateNetworkInterface",
                 "ec2:CreateRoute",
                 "ec2:CreateRouteTable",
-                "ec2:CreateSecurityGroup",
                 "ec2:CreateSubnet",
                 "ec2:CreateTags",
                 "ec2:CreateVpc",
-                "ec2:CreateVpcEndpoint",
-                "ec2:CreateVpcPeeringConnection",
-                "ec2:CreateVpnConnection",
-                "ec2:CreateVpnConnectionRoute",
-                "ec2:CreateVpnGateway",
+                "ec2:CreateVpcEndpoint"
+      ]
+
+      resources = [
+                "arn:aws:ec2:${var.region}:${var.account_id}:subnet/*",
+                "arn:aws:ec2:${var.region}:${var.account_id}:vpc/*",
+                "arn:aws:ec2:${var.region}:${var.account_id}:route-table/*",
+                "arn:aws:ec2:${var.region}:${var.account_id}:natgateway/*",
+                "arn:aws:ec2:${var.region}:${var.account_id}:dhcp-options/*",
+                "arn:aws:ec2:${var.region}:${var.account_id}:internet-gateway/*",
+      ]
+
+
+      condition = {
+        test     = "StringLike"
+        variable = "ec2:ResourceTag/Name"
+        values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
+      }
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+                "ec2:AllocateAddress",
+                "ec2:AssignPrivateIpAddresses",
+                "ec2:AssociateAddress",
+                "ec2:AssociateDhcpOptions",
+                "ec2:AssociateRouteTable",
+                "ec2:AttachClassicLinkVpc",
+                "ec2:AttachInternetGateway",
+                "ec2:AttachNetworkInterface",
                 "ec2:DeleteCustomerGateway",
                 "ec2:DeleteDhcpOptions",
                 "ec2:DeleteFlowLogs",
@@ -62,15 +77,10 @@ data "aws_iam_policy_document" "vpc" {
                 "ec2:DeleteNetworkInterface",
                 "ec2:DeleteRoute",
                 "ec2:DeleteRouteTable",
-                "ec2:DeleteSecurityGroup",
                 "ec2:DeleteSubnet",
                 "ec2:DeleteTags",
                 "ec2:DeleteVpc",
                 "ec2:DeleteVpcEndpoints",
-                "ec2:DeleteVpcPeeringConnection",
-                "ec2:DeleteVpnConnection",
-                "ec2:DeleteVpnConnectionRoute",
-                "ec2:DeleteVpnGateway",
                 "ec2:DescribeAddresses",
                 "ec2:DescribeAvailabilityZones",
                 "ec2:DescribeClassicLinkInstances",
@@ -87,21 +97,16 @@ data "aws_iam_policy_document" "vpc" {
                 "ec2:DescribeNetworkInterfaces",
                 "ec2:DescribePrefixLists",
                 "ec2:DescribeRouteTables",
-                "ec2:DescribeSecurityGroups",
                 "ec2:DescribeSubnets",
                 "ec2:DescribeTags",
                 "ec2:DescribeVpcAttribute",
                 "ec2:DescribeVpcClassicLink",
                 "ec2:DescribeVpcEndpoints",
                 "ec2:DescribeVpcEndpointServices",
-                "ec2:DescribeVpcPeeringConnections",
                 "ec2:DescribeVpcs",
-                "ec2:DescribeVpnConnections",
-                "ec2:DescribeVpnGateways",
                 "ec2:DetachClassicLinkVpc",
                 "ec2:DetachInternetGateway",
                 "ec2:DetachNetworkInterface",
-                "ec2:DetachVpnGateway",
                 "ec2:DisableVgwRoutePropagation",
                 "ec2:DisableVpcClassicLink",
                 "ec2:DisassociateAddress",
@@ -113,7 +118,6 @@ data "aws_iam_policy_document" "vpc" {
                 "ec2:ModifyVpcAttribute",
                 "ec2:ModifyVpcEndpoint",
                 "ec2:MoveAddressToVpc",
-                "ec2:RejectVpcPeeringConnection",
                 "ec2:ReleaseAddress",
                 "ec2:ReplaceNetworkAclAssociation",
                 "ec2:ReplaceNetworkAclEntry",
@@ -121,8 +125,6 @@ data "aws_iam_policy_document" "vpc" {
                 "ec2:ReplaceRouteTableAssociation",
                 "ec2:ResetNetworkInterfaceAttribute",
                 "ec2:RestoreAddressToClassic",
-                "ec2:RevokeSecurityGroupEgress",
-                "ec2:RevokeSecurityGroupIngress",
                 "ec2:UnassignPrivateIpAddresses"
     ]
 
@@ -138,7 +140,7 @@ data "aws_iam_policy_document" "vpc" {
     condition = {
       test     = "StringLike"
       variable = "ec2:ResourceTag/Name"
-      values   = ["default-subnet-*", "${coalesce(var.resources, "${var.prefix}-*")}"]
+      values   = ["${coalesce(var.resources, "${var.prefix}-*")}"]
     }
 
   }
