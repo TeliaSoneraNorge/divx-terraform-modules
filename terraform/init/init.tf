318e9f78fc7e1f3a81b0c582f4e004ca81317309
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-01-22 15:14:03 +0100

    Added an alias to the key used for encrypting the terraform state to make it easier to manage keys in console.

--- terraform/init/init.tf
+++ terraform/init/init.tf
@@ -62,6 +62,11 @@ resource "aws_kms_key" "encrypt" {
   }
 }
 
+resource "aws_kms_alias" "encrypt-alias" {
+  name          = "alias/terraform-state-encryption-key"
+  target_key_id = "${aws_kms_key.encrypt.key_id}"
+}
+
 # ------------------------------------------------------------------------------
 # Output
 # ------------------------------------------------------------------------------
