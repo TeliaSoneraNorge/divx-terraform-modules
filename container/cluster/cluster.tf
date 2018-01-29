85438c0e2fd5d5a5387a183dbc103c5773042e12
itsdalmo <kristian@doingit.no>
2018-01-29 15:53:00 +0100

    Switch cluster module over to using Amazons ECS optimized AMI.

--- container/cluster/cluster.tf
+++ container/cluster/cluster.tf
@@ -14,14 +14,9 @@ variable "subnet_ids" {
   type        = "list"
 }
 
-variable "image_version" {
-  description = "Docker image version."
-  default     = "latest"
-}
-
 variable "instance_ami" {
-  description = "ID of a CoreOS AMI for the instances."
-  default     = "ami-32d1474b"
+  description = "ID of a Amazon Linux ECS optimized AMI for the instances."
+  default     = "ami-1d46df64"
 }
 
 variable "instance_type" {
@@ -80,11 +75,11 @@ data "template_file" "main" {
   template = "${file("${path.module}/cloud-config.yml")}"
 
   vars {
-    aws_region         = "${data.aws_region.current.name}"
+    region             = "${data.aws_region.current.name}"
+    stack_name         = "${var.prefix}-cluster-asg"
     ecs_cluster_name   = "${aws_ecs_cluster.main.name}"
     ecs_log_level      = "${var.ecs_log_level}"
-    ecs_agent_version  = "${var.image_version}"
-    ecs_log_group_name = "${aws_cloudwatch_log_group.main.name}"
+    # ecs_log_group_name = "${aws_cloudwatch_log_group.main.name}"
   }
 }
 
