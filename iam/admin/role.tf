85a347e1a81f493a47f4a62ac6b8208d568d9c58
itsdalmo <kristian@doingit.no>
2018-02-14 08:50:22 +0100

    Added MFA window to admin role.

--- iam/admin/role.tf
+++ iam/admin/role.tf
@@ -9,6 +9,11 @@ variable "trusted_account" {
   description = "ID of the account which is trusted with access to assume this role."
 }
 
+variable "mfa_window" {
+  description = "A window in time (hours) after MFA authenticating where the user is allowed to assume the role."
+  default     = "1"
+}
+
 variable "users" {
   type        = "list"
   description = "List of users in the trusted account which will be allowed to assume this role."
@@ -22,6 +27,7 @@ module "role" {
 
   prefix          = "${var.prefix}-admin"
   trusted_account = "${var.trusted_account}"
+  mfa_window      = "${var.mfa_window}"
   users           = "${var.users}"
 }
 
